d47f822e8a034fe614527b3f6759e7b7862a7bcd
ahmed.magdy <amagsid@gmail.com>
2024-05-15 20:05:30 +0200

    edited tailwind config - added global styles - added dummy data to dummyData.tsx file - created initial folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
+import Dashboard from './pages/Dashboard';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>this is app</h1>
+      <Dashboard />
     </main>
   );
 }
---
+++ app/pages/Dashboard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return <div className=' text-sky-900 '>This is Dashboard</div>;
+};
+
+export default Dashboard;
