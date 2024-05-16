43596a0d1a9393068462074a5fdacf2096d25f6f
ahmed.magdy <amagsid@gmail.com>
2024-05-16 14:56:47 +0200

    managed to secure a connection showing different servers

--- app/pages/Dashboard.tsx
+++ app/pages/Dashboard.tsx
@@ -1,36 +1,60 @@
 'use client';
 import React, { useEffect, useState } from 'react';
+import { cardio } from 'ldrs';
+import { ping } from 'ldrs';
+import { tailChase } from 'ldrs';
+
+ping.register();
+tailChase.register();
 
 const Dashboard = () => {
   const [loading, setLoading] = useState(true);
 
-  // const socket = new WebSocket('wss://lps-monitoring.up.railway.app/realtime');
-  const socket = new WebSocket('ws://localhost:8080');
+  const socket = new WebSocket('wss://lps-monitoring.up.railway.app/realtime');
 
-  socket.addEventListener('open', (e) => {
-    console.log('opened');
-    setLoading(false);
-    socket.send('decode');
+  socket.onopen = () => {
+    console.log('ws opened on browser');
     socket.send(
       JSON.stringify({
-        x: 254,
-        y: 100,
+        type: 'all',
+        machine: 'server01',
+        subscribe: true,
       })
     );
-  });
+    socket.send(
+      JSON.stringify({
+        type: 'all',
+        machine: 'server02',
+        subscribe: true,
+      })
+    );
+    socket.send(
+      JSON.stringify({
+        type: 'all',
+        machine: 'server03',
+        subscribe: true,
+      })
+    );
+    setLoading(false);
+  };
 
-  // socket.onopen = () => {
-  //   console.log('ws opened on browser');
-  //   socket.send('hello world');
-  //   setLoading(false);
-  // };
-
-  // socket.onmessage = (message) => {
-  //   console.log(`message received`, message.data);
-  // };
+  socket.onmessage = (message) => {
+    console.log(`message received`, message.data);
+  };
   return (
     <div className=' text-sky-900 '>
-      <h1 id='wsstatus'> {loading ? 'loading' : 'connection established'} </h1>
+      <div>
+        {' '}
+        {loading ? (
+          <l-tail-chase size='40' speed='1.75' color='black'></l-tail-chase>
+        ) : (
+          <div className='flex items-center align-center'>
+            {' '}
+            <h6>connection ongoing</h6>
+            <l-ping size='45' speed='2' color='black'></l-ping>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
